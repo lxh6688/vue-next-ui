3b46928228a394b6c8636999060bbe2f4dc91886
Lxh6688 <1272662790@qq.com>
2021-12-04 20:41:09 +0800

    基本完成官网框架

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,11 @@
-import {createWebHashHistory, createRouter} from 'vue-router'
+import { createWebHashHistory, createRouter } from 'vue-router'
 import Home from './views/Home.vue'
 import Doc from './views/Doc.vue'
 import SwitchDemo from './components/SwitchDemo.vue'
+import ButtonDemo from "./components/ButtonDemo.vue";
+import DialogDemo from "./components/DialogDemo.vue";
+import TabsDemo from "./components/TabsDemo.vue";
+import DocDemo from "./components/DocDemo.vue";
 
 const history = createWebHashHistory()
 export const router = createRouter({
@@ -11,11 +15,14 @@ export const router = createRouter({
       path: '/', component: Home
     },
     {
-      path: '/Doc', component: Doc, children: [
-        {
-          path: 'switch', component: SwitchDemo
-        },
-      ]
+      path: '/Doc', component: Doc,
+      children: [
+        { path: "", component: DocDemo },
+        { path: "switch", component: SwitchDemo },
+        { path: "button", component: ButtonDemo },
+        { path: "dialog", component: DialogDemo },
+        { path: "tabs", component: TabsDemo },
+      ],
     },
   ]
 })
