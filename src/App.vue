284ead521935f37d15d68c79d86afa6876bdebcb
Lxh6688 <1272662790@qq.com>
2021-12-04 20:28:30 +0800

    路由切换之后关闭 aside

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,20 @@
 </template>
 
 <script lang="ts">
-import { ref } from 'vue'
-import { provide } from 'vue'
+import { ref, provide } from "vue";
+import { router } from "./router";
 
 export default {
-  name: 'App',
+  name: "App",
   setup() {
-    const width = document.documentElement.clientWidth
-    const menuVisible = ref(width <= 500 ? false : true)
-    provide("menuVisible", menuVisible)
+    const width = document.documentElement.clientWidth;
+    const menuVisible = ref(width <= 500 ? false : true);
+    provide("menuVisible", menuVisible);
+    router.afterEach(() => {
+      if (width <= 500) {
+        menuVisible.value = false;
+      }
+    });
   },
-}
+};
 </script>
