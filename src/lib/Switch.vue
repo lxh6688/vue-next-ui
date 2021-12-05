d5c3632be118dc1ed0ad4a21aa7e243bb068855a
Lxh6688 <1272662790@qq.com>
2021-12-05 15:13:26 +0800

    添加动画效果

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,19 +1,19 @@
 <template>
-  <button @click="toggle" :class="{checked}">
+  <button @click="toggle" :class="{ checked }">
     <span></span>
   </button>
 </template>
 <script lang="ts">
-import { ref } from 'vue'
+import { ref } from "vue";
 export default {
- setup(){
-   const checked = ref(false)
-   const toggle = ()=>{
-     checked.value = !checked.value
-   }
-   return {checked, toggle}
- } 
-}
+  setup() {
+    const checked = ref(false);
+    const toggle = () => {
+      checked.value = !checked.value;
+    };
+    return { checked, toggle };
+  },
+};
 </script>
 <style lang="scss" scoped>
 $h: 22px;
@@ -34,11 +34,15 @@ span {
   width: $h2;
   background: white;
   border-radius: $h2 / 2;
+  transition: left 250ms;
 }
 button.checked {
   background: blue;
 }
 button.checked > span {
   left: calc(100% - #{$h2} - 2px);
+}
+button:focus {
+  outline: none;
 }
 </style>
