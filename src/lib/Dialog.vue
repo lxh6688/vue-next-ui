b3c52840370b905299be1a02087ca76c7a8530d3
Lxh6688 <1272662790@qq.com>
2021-12-07 17:13:57 +0800

    第二种方式创建 Dialog

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -59,6 +59,7 @@ export default {
     };
     const cancel = () => {
       context.emit("cancel");
+      props.cancel && props.cancel()
       close();
     };
     return { close, onClickOverlay, ok, cancel };
