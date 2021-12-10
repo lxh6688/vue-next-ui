9f57042586d59edab4757e65450f6c680ad94597
Lxh6688 <1272662790@qq.com>
2021-12-10 22:55:06 +0800

    实现下划线滑动

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="gulu-tabs">
-    <div class="gulu-tabs-nav">
+    <div class="gulu-tabs-nav" ref="container">
       <div
         class="gulu-tabs-nav-item"
         v-for="(t, index) in titles"
         :key="index"
+        :ref="
+          (el) => {
+            if (el) navItems[index] = el;
+          }
+        "
         @click="select(t)"
         :class="{ selected: t === selected }"
       >
         {{ t }}
-        <div class="gulu-tabs-nav-indicator"></div>
       </div>
+      <div class="gulu-tabs-nav-indicator" ref="indicator"></div>
     </div>
     <div class="gulu-tabs-content">
       <component
@@ -24,7 +29,7 @@
   </div>
 </template>
 <script lang="ts">
-import { computed } from "vue";
+import { computed, ref, onMounted, onUpdated } from "vue";
 import Tab from "./Tab.vue";
 export default {
   props: {
@@ -33,6 +38,24 @@ export default {
     },
   },
   setup(props, context) {
+    const navItems = ref<HTMLDivElement[]>([]);
+    const indicator = ref<HTMLDivElement>(null);
+    const container = ref<HTMLDivElement>(null);
+    const x = () => {
+      const divs = navItems.value;
+      const result = divs.filter((div) =>
+        div.classList.contains("selected")
+      )[0];
+      console.log(result);
+      const { width } = result.getBoundingClientRect();
+      indicator.value.style.width = width + "px";
+      const { left: left1 } = container.value.getBoundingClientRect();
+      const { left: left2 } = result.getBoundingClientRect();
+      const left = left2 - left1;
+      indicator.value.style.left = left + "px";
+    };
+    onMounted(x);
+    onUpdated(x);
     const defaults = context.slots.default();
     defaults.forEach((tag) => {
       if (tag.type !== Tab) {
@@ -56,6 +79,9 @@ export default {
       titles,
       current,
       select,
+      navItems,
+      indicator,
+      container,
     };
   },
 };
@@ -69,6 +95,7 @@ $border-color: #d9d9d9;
     display: flex;
     color: $color;
     border-bottom: 1px solid $border-color;
+    position: relative;
     &-item {
       padding: 8px 0;
       margin: 0 16px;
@@ -80,14 +107,14 @@ $border-color: #d9d9d9;
         color: $blue;
       }
     }
-  }
-  &-indicator {
-    position: absolute;
-    height: 3px;
-    background: $blue;
-    left: 0;
-    bottom: -1px;
-    width: 100px;
+    &-indicator {
+      position: absolute;
+      height: 3px;
+      background: $blue;
+      left: 0;
+      bottom: -1px;
+      width: 100px;
+    }
   }
   &-content {
     padding: 8px 0;
