c155cd988785eab1e266a8d28624203bebab3728
Lxh6688 <1272662790@qq.com>
2021-12-10 22:45:10 +0800

    使用 CSS 切换内容

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,16 +1,36 @@
 <template>
   <div class="gulu-tabs">
-  <div class="gulu-tabs-nav">
-    <div class="gulu-tabs-nav-item" v-for="(t,index) in titles" :key="index">{{t}}</div>
-  </div>
-  <div class="gulu-tabs-content">
-    <component class="gulu-tabs-content-item" v-for="(c,index) in defaults" :is="c" :key="index" />
-  </div>
+    <div class="gulu-tabs-nav">
+      <div
+        class="gulu-tabs-nav-item"
+        v-for="(t, index) in titles"
+        :key="index"
+        @click="select(t)"
+        :class="{ selected: t === selected }"
+      >
+        {{ t }}
+      </div>
+    </div>
+    <div class="gulu-tabs-content">
+      <component
+        class="gulu-tabs-content-item"
+        :class="{ selected: c.props.title === selected }"
+        v-for="(c, index) in defaults"
+        :is="c"
+        :key="index"
+      />
+    </div>
   </div>
 </template>
 <script lang="ts">
+import { computed } from "vue";
 import Tab from "./Tab.vue";
 export default {
+  props: {
+    selected: {
+      type: String,
+    },
+  },
   setup(props, context) {
     const defaults = context.slots.default();
     defaults.forEach((tag) => {
@@ -21,9 +41,20 @@ export default {
     const titles = defaults.map((tag) => {
       return tag.props.title;
     });
+    const current = computed(() => {
+      console.log("重新 return");
+      return defaults.filter((tag) => {
+        return tag.props.title === props.selected;
+      })[0];
+    });
+    const select = (title: string) => {
+      context.emit("update:selected", title);
+    };
     return {
       defaults,
       titles,
+      current,
+      select,
     };
   },
 };
@@ -51,6 +82,12 @@ $border-color: #d9d9d9;
   }
   &-content {
     padding: 8px 0;
+    &-item {
+      display: none;
+      &.selected {
+        display: block;
+      }
+    }
   }
 }
 </style>
